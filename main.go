9e819a37eb3e402b9240658257890eacd2bb3b93
nanokomatsu <tomskiyvaleriy@gmail.com>
2025-01-25 01:01:05 +0300

    WEBAPP-1: Lesson 13

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Push it to the limit</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:pivojoppin@gmail.com\">pivojoppin@gmail.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	time.Sleep(3 * time.Second)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		panic(err)
-	}
+	http.ListenAndServe(":3000", nil)
 }
