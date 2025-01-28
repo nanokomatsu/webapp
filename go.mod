ecd331b1ff19ab93effcbc04fe976f72eb352ef7
nanokomatsu <tomskiyvaleriy@gmail.com>
2025-01-28 20:07:27 +0300

    WEBAPP-1: Lesson 24

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module calhoun-basic
 
 go 1.22
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
--- main.go
+++ main.go
@@ -15,6 +15,17 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:pivojoppin@gmail.com\">pivojoppin@gmail.com</a>.")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>	
+ <li><b>Abrakadabra?</b> Yes, there is!</li>
+ <li><b>Which came first: the chicken or the egg?</b> T-Rex!</li>
+ <li><b>How do I contact support?</b> Email us - <a href="mailto:"pivojoppin@gmail.com">pivojoppin@gmail.com</a></li>	
+</ul>
+`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -34,12 +45,15 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
 func main() {
+	http.HandleFunc("/", homeHandler)
 	var router Router
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", router)
