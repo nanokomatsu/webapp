53856ab342e1f86694b3720b8dc05e2e447fa487
nanokomatsu <tomskiyvaleriy@gmail.com>
2025-02-07 20:45:00 +0300

    WEBAPP-1: Lesson 30

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	bio := `<script>alert("Hehe, you have been h4x0r3d!");</script>`
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Push it to the limit</h1>")
+	fmt.Fprint(w, "<h1>Push it to the limit</h1><p>Bio:"+bio+"</p>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
